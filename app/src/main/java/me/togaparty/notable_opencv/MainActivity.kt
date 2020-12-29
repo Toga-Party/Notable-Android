b0b327cd293b83d329310df1f5e1925eac44aa75
Vince <j.vinceeleazar@gmail.com>
2020-12-29 17:25:24 +0800

    Hook-up OpenCV to the current project. Implemented Image Capture and Saving as well.

--- app/src/main/java/me/togaparty/notable_opencv/MainActivity.kt
+++ app/src/main/java/me/togaparty/notable_opencv/MainActivity.kt
@@ -4,33 +4,45 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+//typealias LumaListener = (luma: Double) -> Unit
 
-class MainActivity : AppCompatActivity() {
-    private var imageCapture: ImageCapture? = null
-    private lateinit var outDirectory: File
+class MainActivity : AppCompatActivity(), CameraXConfig.Provider {
+
+    private lateinit var container: ConstraintLayout
+
+    private lateinit var previewFinder: PreviewView
+
+    private lateinit var outputDirectory: File
+
     private lateinit var cameraExecutor: ExecutorService
+
+    private var imageCapture: ImageCapture? = null
+
+    //private var cameraProvider: ProcessCameraProvider? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        container = findViewById(R.id.constraint_layout)
         if (allPermissionsGranted()) {
             startCamera()
         } else {
@@ -39,58 +51,84 @@ class MainActivity : AppCompatActivity() {
 
         val camCaptureButton = findViewById<Button>(R.id.cam_capture_button)
         camCaptureButton.setOnClickListener{takePhoto()}
-        outDirectory = getOutDirectory()
+        previewFinder = findViewById(R.id.camera_view)
+        outputDirectory = getOutDirectory()
+
         cameraExecutor = Executors.newSingleThreadExecutor()
         // Example of a call to a native method
         //findViewById<EditText>(R.id.editText).text = stringFromJNI()
     }
-    private fun startCamera() {
-        val cameraFutureProvider = ProcessCameraProvider.getInstance(this)
-        cameraFutureProvider.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraFutureProvider.get()
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig()
+    }
 
-            val preview = Preview.Builder()
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener( {
+            val cameraProvider = cameraProviderFuture.get()
+            val rotation = previewFinder.display.rotation
+            val metrics = DisplayMetrics().also { previewFinder.display.getRealMetrics(it) }
+            Log.d(TAG, "Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+
+            val preview : Preview = Preview.Builder()
+                    .setTargetRotation(rotation)
                     .build()
                     .also {
-                        val imagePreview = findViewById<PreviewView>(R.id.camera_view)
-                        it.setSurfaceProvider(imagePreview.surfaceProvider)
-                    }
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
+                        it.setSurfaceProvider(previewFinder.surfaceProvider)
+                    }
+            val cameraSelector : CameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+            imageCapture = ImageCapture.Builder()
+                    .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                    .setTargetRotation(rotation)
+                    .build()
+
+            cameraProvider.unbindAll()
             try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this, cameraSelector,preview)
-            } catch (ex: Exception) {
-                Log.e(TAG, "Use case binding failed", ex)
+                cameraProvider.bindToLifecycle(
+                        this as LifecycleOwner, cameraSelector, preview, imageCapture)
+                preview.setSurfaceProvider(previewFinder.surfaceProvider)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
             }
-        }, ContextCompat.getMainExecutor(this)
-        )
+
+        }, ContextCompat.getMainExecutor(this))
+
     }
+
     private fun takePhoto() {
 
-        val imageCapture = imageCapture ?: return //
+        val imageCapture = imageCapture ?: return
 
-        val imageFile = File(outDirectory,
-                SimpleDateFormat(DATE_FORMAT, Locale.TAIWAN)
-                        .format(System.currentTimeMillis()) + ".jpg")
-        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(imageFile).build()
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+                outputDirectory,
+                SimpleDateFormat(FILENAME_FORMAT, Locale.US
+                ).format(System.currentTimeMillis()) + PHOTO_EXTENSION)
 
-        imageCapture.takePicture(outputFileOptions,
-                ContextCompat.getMainExecutor(this),
-                object: ImageCapture.OnImageSavedCallback{
-                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                        val savedURI = Uri.fromFile(imageFile)
-                        val msg = "Photo capture succeeded: $savedURI"
-                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                        Log.d(TAG, msg)
-                    }
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
-                    override fun onError(exception: ImageCaptureException) {
-                        Log.e(TAG, "Photo capture failed: ${exception.message}")
-                    }
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+                outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exc: ImageCaptureException) {
+                Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+            }
 
-                }
-        )
+            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Photo capture succeeded: $savedUri"
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, msg)
+            }
+        })
+
     }
 
     private fun getOutDirectory(): File {
@@ -110,7 +148,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this,
                             "Permissions not granted by the user.",
-                                    Toast.LENGTH_SHORT).show()
+                                   Toast.LENGTH_SHORT).show()
             finish()
         }
     }
@@ -131,7 +169,9 @@ class MainActivity : AppCompatActivity() {
         }
         private const val REQUEST_CODE_PERMISSIONS = 12
         private const val TAG = "Notable:CameraX"
-        private const val DATE_FORMAT = "EEE dd_MM_yyyy HH:mm:ss"
+        private const val FILENAME_FORMAT = "EEE dd_MM_yyyy HH:mm:ss"
+        private const val PHOTO_EXTENSION = ".jpg"
+
         private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA,
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
