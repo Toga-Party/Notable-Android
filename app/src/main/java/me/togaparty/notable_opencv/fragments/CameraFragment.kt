7b23fc5234c25a865a2235f2a4112a9c74bfdf60
Vince <j.vinceeleazar@gmail.com>
2021-01-25 20:47:50 +0800

    Attempt to Implement OpenCv methods (#7). Managed to implement and draw contours on top of image.

--- app/src/main/java/me/togaparty/notable_opencv/fragments/CameraFragment.kt
+++ app/src/main/java/me/togaparty/notable_opencv/fragments/CameraFragment.kt
@@ -25,13 +25,14 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.togaparty.notable_opencv.MainActivity
 import me.togaparty.notable_opencv.R
-import me.togaparty.notable_opencv.utils.*
+import me.togaparty.notable_opencv.utils.convertImageProxyToBitmap
+import me.togaparty.notable_opencv.utils.implement
+import me.togaparty.notable_opencv.utils.toBitmap
+import me.togaparty.notable_opencv.utils.toMat
 import org.opencv.android.InstallCallbackInterface
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.OpenCVLoader.initAsync
-import org.opencv.core.Mat
-import org.opencv.core.MatOfPoint
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -185,32 +186,14 @@ class CameraFragment : Fragment(), CameraXConfig.Provider {
             override fun onCaptureSuccess(image: ImageProxy) {
                 //rudimentary off loading the work
                 GlobalScope.launch(Dispatchers.IO) {
-                    var bitmap = withContext(Dispatchers.IO) { image.convertImageProxyToBitmap() }
+                    val bitmap = withContext(Dispatchers.IO) { image.convertImageProxyToBitmap() }
                     withContext(Dispatchers.IO) {
-                         val byteArrayOutputStream = ByteArrayOutputStream()
+                        val byteArrayOutputStream = ByteArrayOutputStream()
 
-                         with(bitmap.toMat()) {
+                        implement(bitmap.toMat(), bitmap)
+                            .toBitmap()
+                            .compress(Bitmap.CompressFormat.JPEG, 0, byteArrayOutputStream)
 
-                             var gray =  Mat()
-                             this.toGrayScale(bitmap, gray)
-
-                             gray.blur()
-                             gray.erode()
-                             gray.canny(bitmap)
-
-                             var contours: MutableList<MatOfPoint> = ArrayList()
-                             gray.findContours(contours)
-
-                             gray = zeroes(gray)
-                             gray.prepareRectangles(contours)
-                             gray.erode()
-
-                             gray.findContours(contours)
-                             gray.drawRectangles(contours, this)
-
-                             this.toBitmap()
-
-                         }.compress(Bitmap.CompressFormat.JPEG, 0, byteArrayOutputStream)
                         val data = byteArrayOutputStream.toByteArray()
                         val fileOutputStream = FileOutputStream(photoFile)
                         fileOutputStream.write(data)
--- app/src/main/java/me/togaparty/notable_opencv/utils/OpenCVCompat.kt
+++ app/src/main/java/me/togaparty/notable_opencv/utils/OpenCVCompat.kt
@@ -1,15 +1,25 @@
 package me.togaparty.notable_opencv.utils
 
-
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.camera.core.ImageProxy
 import org.opencv.android.Utils
 import org.opencv.core.*
-import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.*
-import java.util.*
-import kotlin.collections.ArrayList
+import java.util.ArrayList
+import kotlin.math.min
+import kotlin.math.max
+
+/*
+fun Mat.threshold(
+        bitmap: Bitmap, thresh: Double = 100.0, max: Double = 255.0,
+        type: Int = THRESH_OTSU, action: (Bitmap) -> Unit
+) {
+    this.toGrayScale(bitmap)
+    threshold(this, this, thresh, max, type)
+    return action(this.toBitmap())
+}
+*/
 
 
 fun ImageProxy.convertImageProxyToBitmap(): Bitmap {
@@ -21,81 +31,12 @@ fun ImageProxy.convertImageProxyToBitmap(): Bitmap {
     return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
 }
 
-
-
-fun Mat.threshold(
-    bitmap: Bitmap, min: Double = 50.00, max: Double = 255.00,
-    type: Int = THRESH_OTSU, action: (Bitmap) -> Unit
-) {
-    this.toGrayScale(bitmap)
-    threshold(this, this, min, max, type)
-    return action(this.toBitmap())
-}
-
-
-fun Mat.findContours(
-    contours: MutableList<MatOfPoint>,
-    hierarchy: Mat = Mat(),
-    retrieval: Int = Imgproc.RETR_EXTERNAL,
-    approx: Int = Imgproc.CHAIN_APPROX_SIMPLE
-) {
-    findContours(this, contours, hierarchy, retrieval, approx)
-}
-
-fun Mat.blur() {
-    blur(this, this, Size(3.0, 3.0))
-}
-fun Mat.erode() {
-    erode(this, this, Mat())
-}
-fun Mat.canny(
-    bitmap: Bitmap, min: Double = 50.0, max: Double = 255.0, aperture: Int = 3,
-    l2Gradient: Boolean = true
-){
-    this.toGrayScale(bitmap)
-    Canny(this, this, min, max, aperture, l2Gradient)
-}
-fun zeroes(src: Mat): Mat {
-    return Mat.zeros(src.rows(), src.cols(), src.type())
-}
-
-fun Mat.drawRectangles(contours: MutableList<MatOfPoint>, src: Mat) {
-    var rect: Rect
-    var big: MutableList<Point> = ArrayList()
-    for (i in 0 until contours.size) {
-        rect = boundingRect(contours.get(i))
-        if (contourArea(contours[i]) > src.cols() * src.rows() / 8) {
-
-            rectangle(src, rect, Scalar(255.0, 0.0, 0.0), 2)
-        } else {
-            big.add(Point(rect.x.toDouble(), rect.y.toDouble()))
-            big.add(Point(
-                (rect.x + rect.width).toDouble(),
-                (rect.y + rect.height).toDouble())
-            )
-        }
-    }
-    var source = MatOfPoint2f()
-    source.fromList(big)
-    rect = boundingRect(source)
-    rectangle(src, rect, Scalar(0.0, 255.0, 0.0), 2)
-
-}
-fun Mat.prepareRectangles(contours: MutableList<MatOfPoint>) {
-    for (i in 0 until contours.size) {
-        val rect: Rect = boundingRect(contours.get(i))
-        if (rect.width > 10 || rect.height > 10) {
-            rectangle(this, rect, Scalar(255.00), -1)
-        }
-    }
-}
-
 fun Mat.toGrayScale(bitmap: Bitmap, mat: Mat? = null) {
     Utils.bitmapToMat(bitmap, this)
     mat?.let{
-        Imgproc.cvtColor(this, mat, Imgproc.COLOR_RGB2GRAY)
+        cvtColor(this, mat, COLOR_RGB2GRAY)
     }?: run {
-        Imgproc.cvtColor(this, this, Imgproc.COLOR_RGB2GRAY)
+        cvtColor(this, this, COLOR_RGB2GRAY)
     }
 }
 
@@ -111,3 +52,93 @@ fun Mat.toBitmap(config: Bitmap.Config = Bitmap.Config.ARGB_8888): Bitmap {
     Utils.matToBitmap(this, bitmap)
     return bitmap
 }
+
+fun Mat.findContours(
+        contours: MutableList<MatOfPoint>,
+        hierarchy: Mat = Mat(),
+        retrieval: Int = RETR_EXTERNAL,
+        approx: Int = CHAIN_APPROX_SIMPLE
+) : MutableList<MatOfPoint> {
+    findContours(this, contours, hierarchy, retrieval, approx, Point(0.0, 0.0))
+    return contours
+}
+
+fun Mat.blur() {
+    blur(this, this, Size(3.0, 3.0))
+}
+fun Mat.erode() {
+    erode(this, this, Mat())
+}
+fun Mat.canny(
+        bitmap: Bitmap, thresh: Double = 50.0, max: Double = 255.0, aperture: Int = 3,
+        l2Gradient: Boolean = true
+){
+    this.toGrayScale(bitmap)
+    Canny(this, this, thresh, max, aperture, l2Gradient)
+}
+fun Mat.prepareContours(contours: MutableList<MatOfPoint>) {
+    for (i in 0 until contours.size) {
+        val rect: Rect = boundingRect(contours[i])
+        if (rect.width > 10 || rect.height > 10) {
+            rectangle(this, rect, Scalar(255.00), -1)
+        }
+    }
+}
+fun Mat.drawContours(contours: MutableList<MatOfPoint>, gray: Mat) {
+    var big = Rect()
+    for (i in 0 until contours.size) {
+        if (contourArea(contours[i]) > gray.cols() * gray.rows() / 8) {
+            val rect = boundingRect(contours[i])
+            rectangle(gray, rect, Scalar(255.0, 0.0, 0.0), 2)
+        } else {
+            if (big.height < 1) {
+                big = boundingRect(contours[i])
+            }
+            big = union(big, boundingRect(contours[i]))
+        }
+    }
+    rectangle(this, big, Scalar (0.0, 255.0, 0.0), 2)
+}
+
+fun zeroes(src: Mat): Mat {
+    return Mat.zeros(src.rows(), src.cols(), src.type())
+}
+
+fun implement(src: Mat, bitmap: Bitmap) : Mat {
+    var gray =  Mat()
+    src.toGrayScale(bitmap, gray)
+
+    gray.blur()
+    gray.erode()
+    gray.canny(bitmap)
+    val contours =
+            gray.findContours(ArrayList<MatOfPoint>())
+
+    gray = zeroes(gray)
+    gray.prepareContours(contours)
+    gray.erode()
+
+    gray.findContours(contours)
+    src.drawContours(contours, gray)
+    return src
+}
+fun union(a: Rect, b: Rect) : Rect {
+    if (a.empty()) {
+        a.x = b.x
+        a.y = b.y
+        a.height = b.height
+        a.width = b.width
+    } else if (!b.empty()) {
+
+        val x = min(a.x, b.x)
+        val y = min(a.y, b.y)
+        a.width =  max(a.x + a.width, b.x + b.width) - x
+        a.height = max(a.y + a.height, b.y + b.height) - y
+        a.x = x
+        a.y = y
+
+    }
+    return a
+}
+
+
