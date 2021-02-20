ae04da5fa6c2dad724a4ed48f05c3654fa405898
Vince <201720078@fit.edu.ph>
2021-02-20 16:12:30 +0800

    Attempt to implement Preference settings in App

--- app/src/main/java/me/togaparty/notable_opencv/fragments/SettingsFragment.kt
+++ app/src/main/java/me/togaparty/notable_opencv/fragments/SettingsFragment.kt
@@ -1,23 +1,14 @@
 package me.togaparty.notable_opencv.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import me.togaparty.notable_opencv.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_screen, rootKey);
     }
-
     companion object
 
 
