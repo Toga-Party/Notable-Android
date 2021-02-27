ed5e2940071f6dc2d1f28ef72094fe0ad4e9067c
Mino <j.vinceeleazar@gmail.com>
2021-02-27 19:08:57 +0800

    Revert "glossary (#3)"
    This reverts commit 001692812be4fdb40ead0ef401edd1988ea7f7b8.

--- app/src/main/java/me/togaparty/notable_opencv/model/CategoryItem.kt
+++ app/src/main/java/me/togaparty/notable_opencv/model/CategoryItem.kt
@@ -1,3 +1,3 @@
 package me.togaparty.notable_opencv.model
 
-data class CategoryItem(var itemId:Int, var itemText:String, var itemDefinition:String)
+class CategoryItem(var itemId:Int, var itemText:String)
