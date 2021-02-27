d65db296be1feef6ac04b09726e5bf01436c305c
Vince <j.vinceeleazar@gmail.com>
2021-02-27 16:38:50 +0800

    Yes another commit. We really need to squash the PR after this. Part 2

--- app/src/main/java/me/togaparty/notable_android/data/network/RetrofitWorker.kt
+++ app/src/main/java/me/togaparty/notable_android/data/network/RetrofitWorker.kt
@@ -31,75 +31,75 @@ class RetrofitWorker(val context: Context) {
         ).apply {
             processed = true
         }
-//        val serviceWorker = RetrofitBuilder.getRetrofit()
-//        val filename = image.name.toRequestBody("text/plain".toMediaTypeOrNull())
-//
-//        var imageToSend: MultipartBody.Part?
-//        context.contentResolver.openInputStream(image.imageUrl).use { input ->
-//            val imageType = context.contentResolver.getType(image.imageUrl)
-//             imageToSend = input?.let {
-//
-//                MultipartBody.Part.createFormData(
-//                        "file",
-//                        image.name,
-//                        it.readBytes().toRequestBody(imageType!!.toMediaTypeOrNull()))
-//
-//            }
-//        }
-//        imageToSend?.let { sendImage -> serviceWorker.create(RetrofitService::class.java)
-//            .upload(sendImage, filename)
-//            .enqueue(object : Callback<ResponseBody> {
-//                override fun onResponse(
-//                        call: Call<ResponseBody>,
-//                        response: Response<ResponseBody>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        Log.v("Upload", "success response received")
-//                        ZipInputStream(response.body()?.byteStream()).use { zip ->
-//                            var entry = zip.nextEntry
-//                            while (entry != null) {
-//
-//                                val outputDirectory = MainActivity.externalAppSpecificStorage(context)
-//
-//                                val tempfile = when (File(entry.name).extension) {
-//                                    "png", "jpg" ->
-//                                        File(
-//                                            outputDirectory,
-//                                            File(image.name).nameWithoutExtension +
-//                                                        separator +
-//                                                        "images"
-//                                            ).apply{mkdirs()}
-//                                    else ->
-//                                        File(outputDirectory,
-//                                            File(image.name).nameWithoutExtension +
-//                                                    separator +
-//                                                    File(entry.name).extension
-//                                        ).apply{mkdirs()}
-//                                }
-//                                val send = File(tempfile,entry.name)
-//                                when (send.extension) {
-//                                    "wav" -> image.addWAVFile(send.nameWithoutExtension, Uri.fromFile(send))
-//                                    "png" -> image.addImageFile(send.nameWithoutExtension,Uri.fromFile(send))
-//                                    "txt" -> image.addTextFile(send.nameWithoutExtension,Uri.fromFile(send))
-//                                }
-//                                image.processed = true
-//                                extractFile(zip, FileOutputStream(send))
-//                                entry = zip.nextEntry
-//                            }
-//                        }
-//                        context.toast("Upload successful")
-//                    } else {
-//                        Log.v("Upload", "Upload failed")
-//                        context.toast("Upload failed")
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-//                    Log.e("Upload", "Something went wrong: ${t.printStackTrace()}", t)
-//                    throw Exception("Failed to get response from the server: ${t.printStackTrace()}")
-//                }
-//            })
-//        } ?: throw Exception("Failed to send the image to server")
+        val serviceWorker = RetrofitBuilder.getRetrofit()
+        val filename = image.name.toRequestBody("text/plain".toMediaTypeOrNull())
+
+        var imageToSend: MultipartBody.Part?
+        context.contentResolver.openInputStream(image.imageUrl).use { input ->
+            val imageType = context.contentResolver.getType(image.imageUrl)
+             imageToSend = input?.let {
+
+                MultipartBody.Part.createFormData(
+                        "file",
+                        image.name,
+                        it.readBytes().toRequestBody(imageType!!.toMediaTypeOrNull()))
+
+            }
+        }
+        imageToSend?.let { sendImage -> serviceWorker.create(RetrofitService::class.java)
+            .upload(sendImage, filename)
+            .enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.v("Upload", "success response received")
+                        ZipInputStream(response.body()?.byteStream()).use { zip ->
+                            var entry = zip.nextEntry
+                            while (entry != null) {
+
+                                val outputDirectory = MainActivity.externalAppSpecificStorage(context)
+
+                                val tempfile = when (File(entry.name).extension) {
+                                    "png", "jpg" ->
+                                        File(
+                                            outputDirectory,
+                                            File(image.name).nameWithoutExtension +
+                                                        separator +
+                                                        "images"
+                                            ).apply{mkdirs()}
+                                    else ->
+                                        File(outputDirectory,
+                                            File(image.name).nameWithoutExtension +
+                                                    separator +
+                                                    File(entry.name).extension
+                                        ).apply{mkdirs()}
+                                }
+                                val send = File(tempfile,entry.name)
+                                when (send.extension) {
+                                    "wav" -> image.addWAVFile(send.nameWithoutExtension, Uri.fromFile(send))
+                                    "png" -> image.addImageFile(send.nameWithoutExtension,Uri.fromFile(send))
+                                    "txt" -> image.addTextFile(send.nameWithoutExtension,Uri.fromFile(send))
+                                }
+                                image.processed = true
+                                extractFile(zip, FileOutputStream(send))
+                                entry = zip.nextEntry
+                            }
+                        }
+                        context.toast("Upload successful")
+                    } else {
+                        Log.v("Upload", "Upload failed")
+                        context.toast("Upload failed")
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Log.e("Upload", "Something went wrong: ${t.printStackTrace()}", t)
+                    throw Exception("Failed to get response from the server: ${t.printStackTrace()}")
+                }
+            })
+        } ?: throw Exception("Failed to send the image to server")
         return image
     }
     internal fun extractFile(zipIn: ZipInputStream, fileOutputStream: FileOutputStream) {
