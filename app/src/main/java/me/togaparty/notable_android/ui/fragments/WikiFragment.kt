ce6415b738da1698782aeee60b44025ec5c45055
Vince <201720078@fit.edu.ph>
2021-03-05 11:59:27 +0800

    Added a new json for wiki entries not sure if that's needed. We still need to choose between restructuring the old json or just create a new one.

--- app/src/main/java/me/togaparty/notable_android/ui/fragments/WikiFragment.kt
+++ app/src/main/java/me/togaparty/notable_android/ui/fragments/WikiFragment.kt
@@ -1,59 +1,55 @@
 package me.togaparty.notable_android.ui.fragments
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import me.togaparty.notable_android.R
+import me.togaparty.notable_android.data.files.JsonParser
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WikiFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WikiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var term : String? = null
+    private var note : String? = null
+    private var duration: String? = null
+
+    private var title: String? = null
+    private var definition: String? = null
+    private val jsonParser by lazy {JsonParser.getInstance(requireContext())}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            term = requireArguments().getString("term")
+            note = requireArguments().getString("note")
+            duration = requireArguments().getString("duration")
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wiki, container, false)
+        val pair = term?.let {
+            jsonParser.getNoteAndDefinition(it)
+        }?: Pair("","")
+
+        title = pair.first
+        definition = pair.second
+        return inflater.inflate(R.layout.fragment_glossary_definition, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WikiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                WikiFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val definitionTextView: TextView = view.findViewById(R.id.definition_textview)
+        view.findViewById<TextView>(R.id.term_textview).text = title
+        definitionTextView.text = definition
+        definitionTextView.movementMethod = ScrollingMovementMethod()
     }
+
+    companion object
 }
