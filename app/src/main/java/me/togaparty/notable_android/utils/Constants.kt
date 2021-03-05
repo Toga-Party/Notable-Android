9c2c73e7442f5651129bcc8bb1c00a0ddbbbec6b
aroueterra <aroueterra@gmail.com>
2021-03-05 13:40:29 +0800

    just some textviews

--- app/src/main/java/me/togaparty/notable_android/utils/Constants.kt
+++ app/src/main/java/me/togaparty/notable_android/utils/Constants.kt
@@ -2,7 +2,7 @@
 
 class Constants {
     companion object {
-        private const val baseURL = "https://683001192fc0.ngrok.io/"
+        private const val baseURL = "https://026f0dde2441.ngrok.io/"
         const val URL = baseURL + "predict/"
         const val HEADER = "/predict"
         const val TAG = "Notable!"
