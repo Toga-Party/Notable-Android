f7100d3165a18560119d9b251b3aed775a9cc358
Mino <201720078@fit.edu.ph>
2021-03-10 18:46:09 +0800

    Update Constants.kt

--- app/src/main/java/me/togaparty/notable_android/utils/Constants.kt
+++ app/src/main/java/me/togaparty/notable_android/utils/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
         const val URL = baseURL + "predict/"
         const val HEADER = "/predict"
         const val TAG = "Notable!"
-        const val GITHUB = "https://github.com/Mino-dev/Notable-OpenCV/issues"
+        const val GITHUB = "https://github.com/Toga-Party/Notable-Android/issues"
         const val GLOSSARY_JSON = "glossary.json"
         const val WIKI_JSON = "wiki.json"
     }
