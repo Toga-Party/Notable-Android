23b13a41149f60fc74e31c5a8303d2fde936012f
Vince <201720078@fit.edu.ph>
2021-03-11 17:15:16 +0800

    Getting ready for release.

--- app/src/main/java/me/togaparty/notable_android/utils/Constants.kt
+++ app/src/main/java/me/togaparty/notable_android/utils/Constants.kt
@@ -2,7 +2,7 @@
 
 class Constants {
     companion object {
-        private const val baseURL = "https://026f0dde2441.ngrok.io/"
+        private const val baseURL = "https://a4fcd053daa5.ngrok.io/"
         const val URL = baseURL + "predict/"
         const val HEADER = "/predict"
         const val TAG = "Notable!"
